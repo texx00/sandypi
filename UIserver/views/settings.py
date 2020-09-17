af1d512745b213a06c969321709bea8980115fb0
texx00 <texx91@gmail.com>
2020-09-17 20:53:38 +0200

    Fixed shutdown/reboot. Can close issue #5

--- UIserver/views/settings.py
+++ UIserver/views/settings.py
@@ -5,6 +5,8 @@ import shutil
 import json
 from utils import settings_utils
 import os
+from time import sleep
+from threading import Thread
 
 
 @app.route("/manual_control", methods=['GET', 'POST'])
@@ -26,15 +28,18 @@ def settings_page():
 # Reboot the device
 @app.route("/control/reboot", methods=['GET', 'POST'])
 def reboot_device():
-    try:
-        return render_template("preferences/control/reboot.html")
-    finally:
-        os.system("sudo reboot")
+    th = Thread(target=delayed_os_command, args=("sudo reboot",))        # delaying reboot call
+    th.start()
+    return render_template("preferences/control/reboot.html")
 
 # Shutdown the device
 @app.route("/control/shutdown", methods=['GET', 'POST'])
 def shutdown_device():
-    try:
-        return render_template("preferences/control/shutdown.html")
-    finally:
-        os.system("sudo shutdown now")
+    th = Thread(target=delayed_os_command, args=("sudo shutdown now",))  # delaying shutdown call
+    th.start()
+    return render_template("preferences/control/shutdown.html")
+
+def delayed_os_command(command):
+    sleep(15)
+    app.logger.info("Executing command: {}\n".format(command))
+    os.system(command)
