49a3deaf75fb1c207f1f79b83f8043c3d4a8c958
Luca Tessaro <texx91@gmail.com>
2020-10-18 14:37:25 +0200

    React setup for playlists

---
+++ UIserver/static/js/fromjsx/playlist_elements.js
@@ -0,0 +1,13 @@
+/**
+* This file is compiled to plain js by using the babel module.
+* Please, have a look at UIserver/static/js/jsx/readme.md for more info before any modification.
+**/
+
+const elements_list = React.createElement(
+    "div",
+    null,
+    "prova",
+    console.log("prova")
+);
+
+ReactDOM.render(elements_list, document.getElementById('elements_container'));
---
+++ UIserver/static/js/jsx/playlist_elements.jsx
@@ -0,0 +1,8 @@
+
+const elements_list = (
+    <div> 
+        prova
+        {console.log("prova")}
+    </div>);
+
+ReactDOM.render(elements_list, document.getElementById('elements_container'));
