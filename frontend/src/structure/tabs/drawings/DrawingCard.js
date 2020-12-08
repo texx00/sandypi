5c63369b6c6b1a251417cdf78dca97c58a9de745
texx00 <texx91@gmail.com>
2020-12-08 10:42:58 +0100

    Changed cards style: added glass effect for the backfround

--- frontend/src/structure/tabs/drawings/DrawingCard.js
+++ frontend/src/structure/tabs/drawings/DrawingCard.js
@@ -22,8 +22,7 @@ class DrawingCard extends Component{
                 <div className="border-0 bg-black rounded text-dark clickable center p-0">
                     <img className="card-img-top rounded" src={getImgUrl(this.props.drawing.id)} alt="Not available"/>
                     <div className="card-img-overlay h-100 d-flex flex-column justify-content-end p-2">
-                        <div className="card-text text-center text-dark p-1 fade-top"></div>
-                        <div className="card-text text-center text-dark pb-1 bg-primary rounded-bottom">
+                        <div className="card-text text-center text-primary p-1 glass rounded-bottom">
                             {this.props.drawing.filename}
                         </div>
                     </div>
