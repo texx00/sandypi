05ae93f221849907324da52da9a55543c46ec8ba
Luca Tessaro <texx91@gmail.com>
2021-01-17 16:25:26 +0100

    Fixed test

--- server/tests/test_leds.py
+++ server/tests/test_leds.py
@@ -1,5 +1,15 @@
-from server.hw_controller.leds.led_driver import LedDriver
+from server.hw_controller.leds.leds_driver import LedsDriver
+
+def test_led_driver_error():
+    try:
+        ld = LedsDriver()
+    except:
+        assert(True)
 
 def test_led_driver():
-    ld = LedDriver()
+    try:
+        ld = LedsDriver((30,20))
+        assert(True)
+    except:
+        assert(False)
     
