d8a72786b17927d3892e9e3d58be5801f4c4d144
Luca Tessaro <texx91@gmail.com>
2021-07-01 21:55:13 +0200

    Should have fixed the problem with the log files

--- server/utils/logging_utils.py
+++ server/utils/logging_utils.py
@@ -3,10 +3,7 @@ import logging
 from logging.handlers import RotatingFileHandler, QueueHandler, QueueListener
 from queue import Queue
 from multiprocessing import RLock
-
-# https://rob-blackbourn.medium.com/how-to-use-python-logging-queuehandler-with-dictconfig-1e8b1284e27a
-log_queue = Queue(-1)
-log_queue_handler = QueueHandler(log_queue)
+import shutil
 
 # creating a custom multiprocessing rotating file handler
 # https://stackoverflow.com/questions/32099378/python-multiprocessing-logging-queuehandler-with-rotatingfilehandler-file-bein
@@ -17,7 +14,15 @@ class MultiprocessRotatingFileHandler(RotatingFileHandler):
 
     def shouldRollover(self, record):
         with self.lock:
-            super(MultiprocessRotatingFileHandler, self).shouldRollover(record)
+            return super(MultiprocessRotatingFileHandler, self).shouldRollover(record)
+    
+    # not sure why but the .log file was seen already open when it was necessary to rotate to a new file.
+    # instead of renaming the file now I'm copying the entire file to the new log.1 file and the clear the original .log file
+    # this is for sure not the best solution but it looks like it is working now
+    def rotate(self, source, dest):
+        shutil.copyfile(source, dest)
+        f = open(source, 'r+')
+        f.truncate(0)
 
 # fixme the rotating file handler is not working for some reason. should find a different solution. Create a new log file everytime the table is turned on? The file should be cached for some iterations? (5?)
 
@@ -31,6 +36,3 @@ server_file_handler.setFormatter(formatter)
 server_stream_handler = logging.StreamHandler()
 server_stream_handler.setLevel(logging.INFO)
 server_stream_handler.setFormatter(formatter)
-
-queue_listener = QueueListener(log_queue_handler, server_file_handler, server_stream_handler)
-queue_listener.start()
