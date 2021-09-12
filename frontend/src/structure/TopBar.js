b9f0bdeaaa53f1b356d50cdef422b31f2c1a5b64
texx00 <texx91@gmail.com>
2021-09-12 09:42:06 +0200

    Layout fixes

--- frontend/src/structure/TopBar.js
+++ frontend/src/structure/TopBar.js
@@ -6,7 +6,7 @@ import IconButton from '../components/IconButton';
 
 import QueueControls from './tabs/queue/QueueControls';
 
-import { showBack } from './tabs/selector';
+import { getTab, showBack } from './tabs/selector';
 import { setTab, tabBack } from './tabs/Tabs.slice';
 import { showLEDs, systemIsLinux } from './tabs/settings/selector';
 import { settingsRebootSystem, settingsShutdownSystem } from '../sockets/sEmits';
@@ -15,7 +15,8 @@ const mapStateToProps = (state) => {
     return { 
         showBack: showBack(state),
         isLinux: systemIsLinux(state),
-        showLEDs: showLEDs(state)
+        showLEDs: showLEDs(state),
+        selectedTab: getTab(state)
     }
 }
 
@@ -60,7 +61,11 @@ class TopBar extends Component{
 
     renderLEDsTab(){
         if (this.props.showLEDs)
-            return <Nav.Link key={5} onClick={()=>{this.props.handleTab("leds")}}>LEDs</Nav.Link>
+            return <Nav.Link className={"h5" + (this.props.selectedTab === "leds" ? " highlight-tab" : "")}
+                key={5} 
+                onClick={()=>{this.props.handleTab("leds")}}>
+                    LEDs
+            </Nav.Link>
         else return "";
     }
 
@@ -74,9 +79,26 @@ class TopBar extends Component{
                     <Navbar.Toggle aria-controls="topbar-nav" />
                     <Navbar.Collapse id="topbar-nav" className="max-width">
                         <Nav className="mr-auto">
-                            <Nav.Link key={2} onClick={()=>{this.props.handleTab("drawings")}}>Drawings</Nav.Link>
-                            <Nav.Link key={3} onClick={()=>{this.props.handleTab("playlists")}}>Playlists</Nav.Link>
-                            <Nav.Link key={4} onClick={()=>{this.props.handleTab("manual")}}>Manual control</Nav.Link>
+                            <Nav.Link className={"h5" + (this.props.selectedTab === "home" ? " highlight-tab" : "")} 
+                                key={1} 
+                                onClick={()=>{this.props.handleTab("home")}}>
+                                    Home
+                            </Nav.Link>
+                            <Nav.Link className={"h5" + (this.props.selectedTab === "drawings" ? " highlight-tab" : "")} 
+                                key={2} 
+                                onClick={()=>{this.props.handleTab("drawings")}}>
+                                    Drawings
+                            </Nav.Link>
+                            <Nav.Link className={"h5" + (this.props.selectedTab === "playlists" ? " highlight-tab" : "")} 
+                                key={3} 
+                                onClick={()=>{this.props.handleTab("playlists")}}>
+                                    Playlists
+                            </Nav.Link>
+                            <Nav.Link className={"h5" + (this.props.selectedTab === "manual" ? " highlight-tab" : "")}
+                                key={4} 
+                                onClick={()=>{this.props.handleTab("manual")}}>
+                                    Manual control
+                            </Nav.Link>
                             {this.renderLEDsTab()}
                             {this.renderBack()}
                         </Nav>
--- frontend/src/utils/colorUtils.js
+++ frontend/src/utils/colorUtils.js
@@ -17,13 +17,13 @@ function hexToRGB(h) {
     let r = 0, g = 0, b = 0;
   
     // 3 digits
-    if (h.length == 4) {
+    if (h.length === 4) {
       r = "0x" + h[1] + h[1];
       g = "0x" + h[2] + h[2];
       b = "0x" + h[3] + h[3];
   
     // 6 digits
-    } else if (h.length == 7) {
+    } else if (h.length === 7) {
       r = "0x" + h[1] + h[2];
       g = "0x" + h[3] + h[4];
       b = "0x" + h[5] + h[6];
