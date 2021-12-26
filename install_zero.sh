3b65d6911156847dcc3c8c5a7a9cfe592bad77a2
texx00 <texx91@gmail.com>
2021-12-26 11:47:56 +0100

    Adding gunicorn to docker image

--- install_zero.sh
+++
@@ -1,8 +0,0 @@
-echo '----- Installing python dependencies -----'
-python3 -m pip install -r requirements.txt
-
-echo '\n\n----- Upgrading database -----'
-flask db upgrade
-
-echo '\n\n----- Installing app -----'
-sudo python3 setup.py install
