7bf2d0d3bb38a3aef5d4fcbc3c6ed6444b6d8e50
Luca Tessaro <texx91@gmail.com>
2022-03-31 14:38:33 +0200

    Fixes for the docker build

--- docker/start_server.sh
+++ docker/start_server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # check if .env files needs to be replaced
-python check_prestart.py
+python check_prestart.py 
 
 # Need to update the flask db at runtime
-flask db upgrade
+flask db upgrade 
 
 # TODO setup a production server
 # starts the server
-flask run --host=0.0.0.0
+flask run --host=0.0.0.0 
